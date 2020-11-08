ff259dff5e41cd8d747aec289e25b07df9245727
GitPushMe <nikita.demin.1996@inbox.ru>
2020-11-08 22:34:17 +0700

    Create file configuration and connect data base ip_cal

---
+++ conf_bd.php
@@ -0,0 +1,9 @@
+﻿<!-- Author: GitPusmMe (nikita.demin.1996@index.ru) -->
+
+<?php
+	$conf = parse_ini_file('config.ini');
+
+	mysql_connect($conf['mysql_host'],$conf['mysql_user'],$conf['mysql_password']) or die('Ошибка подключения к базе данных');
+	mysql_select_db($conf['mysql_database']) or die('Ошибка подключения к базе данных');
+
+?>
