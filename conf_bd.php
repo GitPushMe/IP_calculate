14bb2a441bc9046c536fcbf4cb8ee546f9faaf40
romandr154 <roma@romandr.ru>
2020-12-07 00:22:32 +0700

    Fix connect to BD (add UTF8)

--- conf_bd.php
+++ conf_bd.php
@@ -4,6 +4,7 @@
 	$conf = parse_ini_file('config.ini');
 
 	mysql_connect($conf['mysql_host'],$conf['mysql_user'],$conf['mysql_password']) or die('Ошибка подключения к базе данных');
+	mysql_query("SET NAMES 'utf8'");
 	mysql_select_db($conf['mysql_database']) or die('Ошибка подключения к базе данных');
 
 ?>
